63c20274de8f3877a0fbba43fa63d761e4f42c47
Mikhail Vaysman <9996113+github@gmail.com>
2010-03-13 03:30:15 +0300

    user model added

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+describe User do
+  before(:each) do
+    @valid_attributes = {
+      :name => ,
+      :password => ,
+      :login => ,
+      :email => "value for email"
+    }
+  end
+
+  it "should create a new instance given valid attributes" do
+    User.create!(@valid_attributes)
+  end
+end
